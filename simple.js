829028b4a8eb60dd3fd3c60bc071e545c90c6a54
Henning Haldsrud <haldsrud@hotmail.com>
2025-02-15 17:06:32 +0100

    Name

--- simple.js
+++ simple.js
@@ -1,1 +1,6 @@
-console.log("Hello world!");
+$.ajax ({
+    url: 'https://api.github.com/repos/Henning-NOR/MyFirstRepo',
+    success: function(response) {
+        console.log(response);
+    }
+})
